35c63164ec62a814f275a57ad55035ddd876abf1
GR <GR@GRN-304-05>
2024-07-16 12:50:20 +0900

    1234

--- src/main/java/org/hj/mapper/c.java
+++
@@ -1,9 +0,0 @@
-package org.hj.mapper;
-
-public class c {
-
-	public c() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- src/main/java/org/hj/model/c.java
+++
@@ -1,9 +0,0 @@
-package org.hj.model;
-
-public class c {
-
-	public c() {
-		// TODO Auto-generated constructor stub
-	}
-
-}

959258314c9c02cbf360de6b076a52a3ce657ae4
GR <GR@GRN-304-05>
2024-07-19 12:47:35 +0900

    로그인 화면 및 관리자/일반 유저 로그인 로직 추가

--- src/main/java/org/hj/controller/LoginController.java
+++ src/main/java/org/hj/controller/LoginController.java
@@ -12,44 +12,72 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
-	
+
 	@Autowired
 	LoginM_Service lms;
-	
+
 	@Autowired
 	LoginU_Service lus;
 
-	@GetMapping("/login")
-	public String goLogin() {
-		return "login";
-	}
-
-
-	@GetMapping ("/loginTest")
-	public String loginTest(){
-		return "loginTest";
+	@GetMapping("/")
+	public String goLogin(HttpSession session) {
+		// 로그인 되어 있는 상태라면 로그인 페이지로 못가게끔
+		if (session.getAttribute("id") != null) {
+			if(session.getAttribute("id").equals("master")) {
+				System.out.println("관리자 로그인 상태");
+				return "redirect:/productRegister";
+			}else {
+				System.out.println("일반 로그인 상태");
+				return "redirect:/loginTest";
+			}			
+		} else {
+			System.out.println("로그인 전 상태");
+			return "login";
+		}
 	}
 	
+	@GetMapping("/productRegister")
+	public String goProductRegister(HttpSession session) {
+		if(session.getAttribute("id") != null) {
+			return "productRegister";
+		}else {
+			return "redirect:/login";
+		}
+		
+	}
+	
+	@GetMapping("/loginTest")
+	public String loginTest() {
+		return "loginTest";
+	}
+
+	@GetMapping("/plesaeRegister")
+	public String goPlesaeRegister() {
+		return "plesaeRegister";
+	}
+
 	@PostMapping("/login")
 	public String LoginM(UserDto ud, HttpSession session) {
 
 		System.out.println(ud);
-		
+
 		// 일반 유저 로그인 성공 시
-		if(lus.loginU(ud) != null) {
+		if (lus.loginU(ud) != null) {
 			session.setAttribute("id", lus.loginU(ud).getU_id());
 			System.out.println("일반 로그인 성공");
+			return "redirect:/loginTest"; // 쇼핑몰 메인으로 
 		}
-		
+
 		// 마스터 로그인 성공 시
-		else if(lms.loginM(ud)!=null) {
+		else if (lms.loginM(ud) != null) {
 			session.setAttribute("id", lms.loginM(ud).getM_id());
 			System.out.println("마스터 로그인 성공");
-			
-		}else {
+			return "redirect:/productRegister"; // 관리자 페이지로
+			// 로그인 실패 시
+		} else {
 			System.out.println("로그인 실패");
+			return "redirect:/plesaeRegister";
 		}
-		
-		return "redirect:/loginTest";
+
 	}
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,24 +1,26 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<link href="../resources/css/login.css" rel="stylesheet">
+
 </head>
 <body>
-<form action="/login" method="post">
+	<header> HEADER </header>
+	<form action="/login" method="post">
 		<table>
-			<tr>
+			<tr class="loginInput">
 				<td>아이디</td>
-				<td><input type="text" id="u_id" name = "u_id"></td>
+				<td><input type="text" id="u_id" name="u_id" placeholder="아이디"></td>
 			</tr>
-			<tr>
+			<tr class="loginInput">
 				<td>비밀번호</td>
-				<td><input type="text" id="u_pw" name = "u_pw"></td>
+				<td><input type="password" id="u_pw" name="u_pw" placeholder="비밀번호"></td>
 			</tr>
-			<tr>			
-				<td><input type="submit" value="로그인"></td>	
+			<tr class="loginInput">
+				<td colspan="2""><input id="loginB" type="submit" value="로그인"></td>
 			</tr>
 		</table>
 	</form>
